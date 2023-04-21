0f721469b4a54bd1f0901b21e2b13018d27cb8fc
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2023-04-21 04:16:33 +0300

    wef

--- dist/payment/dto/payment.dto.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class PaymentDto {
-    amount: number;
-}
--- dist/user/user.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserService } from './user.service';
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UserService);
-    getProfile(_id: string): Promise<import("@typegoose/typegoose").DocumentType<import("./user.model").UserModel, import("@typegoose/typegoose/lib/types").BeAnObject>>;
-    updateProfile(_id: string, data: {
-        have_read: boolean;
-    }): Promise<boolean>;
-}
